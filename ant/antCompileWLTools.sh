805d72800fd92ca572556f2b6e5db36aa737e992
Gilles Mercier <gilles.mercier@dfo-mpo.gc.ca>
2023-08-15 16:04:13 +0000

    Added the HDFql wrapper code and lib

--- ant/antCompileWLTools.sh
+++ ant/antCompileWLTools.sh
@@ -6,19 +6,29 @@ javaClassesMainDestDir=${javaCodeBaseMainDir}/lib
 
 export CLASSPATH=${javaClassesMainDestDir}:${javaCodeBaseMainDir}:${CLASSPATH}
 
-ant -buildfile ${javaCodeBaseMainDir}/ant/antCompLib.ant
+ant -buildfile ${javaCodeBaseMainDir}/ant/antCompLib.xml
 
 if [ ${?} -ne 0 ];
 then
-  echo "ant -buildfile ${javaCodeBaseMainDir}/ant/antCompLib.ant FAILED!!"
+  echo "ant -buildfile ${javaCodeBaseMainDir}/ant/antCompLib.xml FAILED!!"
   exit 1
 
 fi
 
-ant -buildfile ${javaCodeBaseMainDir}/ant/antCompMain.ant
+ant -buildfile ${javaCodeBaseMainDir}/ant/antCompHDFql.xml
 
 if [ ${?} -ne 0 ];
 then
-  echo "ant -buildfile ${javaCodeBaseMainDir}/ant/antCompMain.ant FAILED!!"
+  echo "ant -buildfile ${javaCodeBaseMainDir}/ant/antCompHDFql.xml FAILED!!"
+  exit 1
+
+fi
+
+
+ant -buildfile ${javaCodeBaseMainDir}/ant/antCompMain.xml
+
+if [ ${?} -ne 0 ];
+then
+  echo "ant -buildfile ${javaCodeBaseMainDir}/ant/antCompMain.xml FAILED!!"
   exit 1
 fi
