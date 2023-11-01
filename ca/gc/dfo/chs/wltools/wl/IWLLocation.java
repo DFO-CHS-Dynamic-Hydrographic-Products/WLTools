08ade03c56d134a1404a176cd9580491b5e99d95
Gilles Mercier <gilles.mercier@dfo-mpo.gc.ca>
2023-11-01 20:48:04 +0000

    Forgot to commit some other source files.

--- ca/gc/dfo/chs/wltools/wl/IWLLocation.java
+++ ca/gc/dfo/chs/wltools/wl/IWLLocation.java
@@ -16,8 +16,12 @@ public interface IWLLocation {
   String INFO_JSON_LATCOORD_KEY= "lat";
   String INFO_JSON_LONCOORD_KEY= "lon";
 
-  String INFO_JSON_ZCIGLD_CONV_KEY= "zcVsIGLD"; //--- Implies that tha the preductions are referred to IGLD datum
-                                                         //    WL(IGLD) - zcVsIGLD to get the ZC values
+  String INFO_JSON_ZCIGLD_CONV_KEY= "zcVsIGLD"; //--- WL(ZC) = WL(IGLD) - zcVsIGLD to convert from IGLD to ZC values
+                                                //    OR WL(IGLD) = WL(ZC) + zcVsIGLD to convert from ZC to IGLD values
+
+  String INFO_JSON_ZCIGLD_CONV_BOOL_KEY= "convFromZCToIGLD"; // --- To do (or not) the conversion from ZC to the IGLD vertical datum.
+
+  String INFO_JSON_SPINE_FPP_BOOL_KEY= "doSpineFPP";
 
   String INFO_JSON_DICT_KEY= "channelGridPointInfo"; // --- The json dict key for the station info dict
 
