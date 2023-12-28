c694788391095c095ce67830063a24ef42df47e9
Gilles Mercier <gilles.mercier@dfo-mpo.gc.ca>
2023-12-28 18:02:42 +0000

    Added management of the main class bin folder.

--- ant/antCompileWLTools.sh
+++ ant/antCompileWLTools.sh
@@ -3,10 +3,14 @@
 # --- Modify according to the code location context.
 javaCodeBaseMainDir=../
 javaClassesMainDestDir=${javaCodeBaseMainDir}/lib
+javaMainClassDestDir=${javaCodeBaseMainDir}/bin
+
+# --- Create javaMainClassDestDir if not already existing.
+mkdir -p ${javaMainClassDestDir}
 
 # --- force recompilation of the main program source file
-#     (Still need to implement dependencies for ant)
-rm -f ${javaCodeBaseMainDir}/bin/*.class
+#     (TODO: Still need to implement dependencies for ant)
+rm -f ${javaCodeBaseMainDir}/*.class
 
 export CLASSPATH=${javaClassesMainDestDir}:${javaCodeBaseMainDir}:${CLASSPATH}
 
