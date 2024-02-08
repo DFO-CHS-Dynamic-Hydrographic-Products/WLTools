804516c94b50ee6990bee5bd5e748d560220c3bc
Gilles Mercier <gilles.mercier@dfo-mpo.gc.ca>
2024-02-08 22:56:46 +0000

    WIP for getting the WLO data from the IWLS.

--- ca/gc/dfo/chs/wltools/wl/adjustment/WLAdjustmentSpineFPP.java
+++ ca/gc/dfo/chs/wltools/wl/adjustment/WLAdjustmentSpineFPP.java
@@ -13,23 +13,25 @@ import java.util.HashSet;
 import java.util.TreeSet;
 //import java.util.Iterator;
 import java.nio.file.Path;
+import java.net.URLEncoder;
 import java.nio.file.Paths;
 import java.io.InputStream;
 import java.util.SortedSet;
 import java.util.ArrayList;
-import java.util.ListIterator;
+//import java.util.ListIterator;
 import java.nio.file.Files;
 import java.util.Collection;
 import java.util.NavigableSet;
 //import java.nio.file.PathMatcher;
+import java.net.URLConnection;
+import java.io.InputStreamReader;
 import java.nio.file.FileSystems;
 import java.nio.file.DirectoryStream;
 import java.nio.file.StandardCopyOption;
-import java.nio.file.DirectoryIteratorException;
+import java.io.UnsupportedEncodingException;
+//import java.nio.file.DirectoryIteratorException;
 
-//import java.awt.geom.Point2D; //.Double;
-//import java.util.concurrent.ConcurrentHashMap;
-
+// ---
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -103,7 +105,7 @@ final public class WLAdjustmentSpineFPP extends WLAdjustmentSpinePP {
 
     super(IWLAdjustment.Type.SpineFPP,argsMap);
 
-    final String mmi= "mWLAdjustmentSpineFPP ain constructor: ";
+    final String mmi= "WLAdjustmentSpineFPP main constructor: ";
 
     slog.info(mmi+"start");
 
@@ -197,11 +199,56 @@ final public class WLAdjustmentSpineFPP extends WLAdjustmentSpinePP {
       throw new RuntimeException(mmi+" inputFileDldLocalDest -> "+inputFileDldLocalDest+" not found !");
     }
 
-    // --- Now check if we have at least 31 days of data in the future for the S104 DCF8 input file.
+    // --- TODO: Now check if we have at least 31 days of data in the future for the S104 DCF8 input file.
     //     If not we stop the exec.
+    
+    // --- TODO: Tell the HDFql world to use just one thread here.
+    
+    // --- Now get the WLO data from the IWLS for all the tide gauge locations.
+    URLConnection uc= null;
 
-    // --- Tell the HDFql world to use just one thread here. 
-	  
+    String ec1;
+    String ec2;
+
+    try {
+      ec1= URLEncoder.encode("2024-02-06T06%3A00%3A00Z","UTF-8");
+    } catch (UnsupportedEncodingException e) {
+      throw new RuntimeException(mmi+e);
+    }
+
+    try {
+      ec2= URLEncoder.encode("2024-02-08T11%3A00%3A00Z","UTF-8");
+    } catch (UnsupportedEncodingException e) {
+      throw new RuntimeException(mmi+e);
+    }   
+ 
+    //final String es=
+    // "https://api.test.iwls.azure.cloud.dfo-mpo.gc.ca/api/v1/stations/5cebf1e23d0f4a073c4bc0f6/data?time-series-code=wlo&from="+ec1+"&to="+ec2;
+
+    final String es=
+	"https://api.test.iwls.azure.cloud.dfo-mpo.gc.ca/api/v1/stations/5cebf1e23d0f4a073c4bc0f6/data?time-series-code=wlo&from=2024-02-06T06%3A00%3A00Z&to=2024-02-08T11%3A00%3A00Z";
+	
+    try {
+     uc= new URL(es).openConnection();
+    } catch (IOException ioe) {
+      ioe.printStackTrace();	
+      throw new RuntimeException(mmi+ioe+"\nProblem with openConnection()");
+    }
+
+    // --- no neee for a ":" char here after the accept
+    //uc.setRequestProperty("accept ", "*/*");
+
+    InputStream ist= null;
+	//InputStreamReader inputStreamReader= null;
+
+    try {
+      ist= uc.getInputStream();
+	//inputStreamReader= new InputStreamReader(uc.getInputStream());
+    } catch (IOException ioe) {
+      ioe.printStackTrace();    	
+      throw new RuntimeException(mmi+ioe+"\nProblem with new uc.getInputStream()");
+    }
+    
     slog.info(mmi+"end");
     
     slog.info(mmi+"debug exit 0");
