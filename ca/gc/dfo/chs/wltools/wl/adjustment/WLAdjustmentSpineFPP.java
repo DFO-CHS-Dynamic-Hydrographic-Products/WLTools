cd4bc15c42c1de5ece988a48c698e0c10156882d
Gilles Mercier <gilles.mercier@dfo-mpo.gc.ca>
2024-02-08 16:05:00 +0000

    Forgot to add the wl/adjustment/WLAdjustmentSpineFPP.java src file in the previous commit.

---
+++ ca/gc/dfo/chs/wltools/wl/adjustment/WLAdjustmentSpineFPP.java
@@ -0,0 +1,227 @@
+package ca.gc.dfo.chs.wltools.wl.adjustment;
+
+//---
+import java.net.URL;
+import java.io.File;
+import java.util.Map;
+import java.util.Set;
+import java.util.List;
+import java.util.Arrays;
+import java.time.Instant;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.TreeSet;
+//import java.util.Iterator;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.io.InputStream;
+import java.util.SortedSet;
+import java.util.ArrayList;
+import java.util.ListIterator;
+import java.nio.file.Files;
+import java.util.Collection;
+import java.util.NavigableSet;
+//import java.nio.file.PathMatcher;
+import java.nio.file.FileSystems;
+import java.nio.file.DirectoryStream;
+import java.nio.file.StandardCopyOption;
+import java.nio.file.DirectoryIteratorException;
+
+//import java.awt.geom.Point2D; //.Double;
+//import java.util.concurrent.ConcurrentHashMap;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+//---
+import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.JsonValue;
+import javax.json.JsonObject;
+import javax.json.JsonReader;
+
+import java.io.IOException;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+// ---
+import ca.gc.dfo.chs.wltools.WLToolsIO;
+import ca.gc.dfo.chs.wltools.IWLToolsIO;
+import ca.gc.dfo.chs.wltools.util.IHBGeom;
+import ca.gc.dfo.chs.wltools.util.HBCoords;
+import ca.gc.dfo.chs.wltools.wl.IWLLocation;
+import ca.gc.dfo.chs.wltools.tidal.ITidalIO;
+import ca.gc.dfo.chs.wltools.wl.WLMeasurement;
+import ca.gc.dfo.chs.wltools.util.Trigonometry;
+import ca.gc.dfo.chs.wltools.wl.ITideGaugeConfig;
+import ca.gc.dfo.chs.wltools.util.MeasurementCustom;
+//import ca.gc.dfo.chs.wltools.nontidal.stage.StageIO;
+import ca.gc.dfo.chs.wltools.wl.adjustment.IWLAdjustment;
+import ca.gc.dfo.chs.wltools.util.MeasurementCustomBundle;
+import ca.gc.dfo.chs.wltools.wl.adjustment.WLAdjustmentIO;
+import ca.gc.dfo.chs.wltools.wl.adjustment.IWLAdjustmentIO;
+import ca.gc.dfo.chs.wltools.wl.prediction.IWLStationPredIO;
+import ca.gc.dfo.chs.wltools.wl.adjustment.WLAdjustmentSpinePP;
+import ca.gc.dfo.chs.wltools.tidal.nonstationary.INonStationaryIO;
+
+/**
+ * Comments please!
+ */
+final public class WLAdjustmentSpineFPP extends WLAdjustmentSpinePP {
+
+  private final static String whoAmI=
+    "ca.gc.dfo.chs.wltools.wl.adjustment.WLAdjustmentSpineFPP: ";
+
+ /**
+   * Usual class static log utility.
+   */
+  private final static Logger slog= LoggerFactory.getLogger(whoAmI);
+
+  // --- To store the non-ajusted WL predictions OR non-adjusted FMF data at the ship channel point locations that are
+  //     in-between the tide gauges locations being processed (INPUT ONLY) 
+  //private Map<String, MeasurementCustomBundle> scLocsNonAdjData= null;
+
+  // --- To store the non-ajusted WL predictions OR non-adjusted FMF data at the two ship channel point locations that are
+  //     the nearest to the nearest tide gauges locations (INPUT ONLY)
+  //private Map<String, MeasurementCustomBundle>
+  //  tgsNearestSCLocsNonAdjData= new HashMap<String, MeasurementCustomBundle>(2); //null;
+
+  //private String fmfReferenceDateTimeStr= null;
+    
+  /**
+   * Comments please!
+   */
+  public WLAdjustmentSpineFPP() {
+    super();
+
+    //this.wlOriginalData=
+    //  this.wlAdjustedData= null;
+  }
+
+  // ---
+  public WLAdjustmentSpineFPP(/*@NotNull*/ final HashMap<String,String> argsMap) {
+
+    super(IWLAdjustment.Type.SpineFPP,argsMap);
+
+    final String mmi= "mWLAdjustmentSpineFPP ain constructor: ";
+
+    slog.info(mmi+"start");
+
+    // try {
+    //   this.lowerSideScLocStrId.length();	
+    // } catch (NullPointerException npe) {
+    //   throw new RuntimeException(mmi+npe);
+    // }
+
+    // try {
+    //   this.upperSideScLocStrId.length();	
+    // } catch (NullPointerException npe) {
+    //   throw new RuntimeException(mmi+npe);
+    // }
+
+    try {
+      WLToolsIO.getOutputDataFormat();
+    } catch (NullPointerException npe) {
+      throw new RuntimeException(mmi+npe+"WLToolsIO.getOutputDataFormat() cannot be null here !");
+    }
+
+    // --- Verify the output file(s) format before going further
+    //if (!WLToolsIO.getOutputDataFormat().equals(IWLToolsIO.Format.DHP_S104_DCF8.name())) {
+    
+    if (!WLToolsIO.getOutputDataFormat().equals(IWLToolsIO.Format.LEGACY_ASCII.name())) {
+      throw new RuntimeException(mmi+"Invalid output file(s) data format -> "+
+				 WLToolsIO.getOutputDataFormat()+" for the SpineFPP adjustment type sub-tool!");
+    }
+
+    if (!argsMap.containsKey("--localDownloadDirectory")) {
+      throw new RuntimeException(mmi+
+	"Must have the --localDownloadDirectory=<path of the folder where to download data> defined in the argsMap !!");
+    }
+
+    final String localDownloadDirectory= argsMap.get("--localDownloadDirectory");
+
+    slog.info(mmi+"localDownloadDirectory="+localDownloadDirectory);
+
+    if (!argsMap.containsKey("--inputFileURLToGet")) {
+      throw new RuntimeException(mmi+
+	"Must have the --inputFileURLToGet=<Complete https URL of the S104 DCF8 file that has to be downloaded> defined in the argsMap !!");
+    }
+
+    final String inputFileURLToGet= argsMap.get("--inputFileURLToGet");
+
+    slog.info(mmi+"inputFileURLToGet="+inputFileURLToGet);
+    
+    final String inputFileNameToGet= new File(inputFileURLToGet).getName();
+
+    slog.info(mmi+"inputFileNameToGet="+inputFileNameToGet);
+    //slog.info(mmi+"debug exit 0");
+    //System.exit(0);
+
+    final String inputFileDldLocalDest= localDownloadDirectory + File.separator + inputFileNameToGet;
+
+    slog.info(mmi+"inputFileDldLocalDest="+inputFileDldLocalDest);
+    
+    boolean success= true;
+    
+    // --- 
+    try {
+      Files.copy( new URL(inputFileURLToGet).openStream(),
+		  Paths.get(inputFileDldLocalDest), StandardCopyOption.REPLACE_EXISTING);
+      
+    } catch (IOException ioe) {
+      slog.warn(mmi+" Problem with the 1st try for the "+inputFileURLToGet+" download !");
+      success= false;		
+    }
+
+    slog.info(mmi+"1st download try done, success="+success);
+
+    if (!success) {	
+      try {
+        Files.copy( new URL(inputFileURLToGet).openStream(),
+		    Paths.get(inputFileDldLocalDest), StandardCopyOption.REPLACE_EXISTING);
+	
+      } catch (IOException ioe) {
+	//throw new RuntimeException(mmi+ioe+" Also a problem with the 2nd try for the "+inputFileURLToGet+" download !");
+        slog.warn(mmi+" Also a problem with the 2nd try for the "+inputFileURLToGet+" download !");
+      }	
+    }
+
+    if (success) {
+      slog.info(mmi+"Success for the "+inputFileURLToGet+" download to local destination: "+inputFileDldLocalDest);
+    } else {
+      slog.warn(mmi+"Had a problem to download the S104 DCF8 input file, will use the previous input file!");
+    }
+
+    // --- Check for the S104 DCF8 input file existence on local disk whatever its download has succeded or not
+    if (!WLToolsIO.checkForFileExistence(inputFileDldLocalDest)) {
+      throw new RuntimeException(mmi+" inputFileDldLocalDest -> "+inputFileDldLocalDest+" not found !");
+    }
+
+    // --- Now check if we have at least 31 days of data in the future for the S104 DCF8 input file.
+    //     If not we stop the exec.
+
+    // --- Tell the HDFql world to use just one thread here. 
+	  
+    slog.info(mmi+"end");
+    
+    slog.info(mmi+"debug exit 0");
+    System.exit(0);
+  }
+
+  // --- 
+  final public List<MeasurementCustom> getAdjustment(final String outputDirectory) {
+
+    final String mmi= "getAdjustment: ";
+      
+    List<MeasurementCustom> ret= null;
+    
+    slog.info(mmi+"start");
+    
+    
+    slog.info(mmi+"end");
+    slog.info(mmi+"debug exit 0");
+    System.exit(0);     
+     
+    return ret;
+  }
+}
