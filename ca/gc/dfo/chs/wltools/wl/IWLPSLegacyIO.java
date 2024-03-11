175c2f4cfa8ae988ac0b37284ddb72ddb7799941
Gilles Mercier <gilles.mercier@dfo-mpo.gc.ca>
2024-03-11 14:13:30 -0400

    The last timestamp for Spine API data was missing.

--- ca/gc/dfo/chs/wltools/wl/IWLPSLegacyIO.java
+++ ca/gc/dfo/chs/wltools/wl/IWLPSLegacyIO.java
@@ -105,9 +105,14 @@ public final class IWLPSLegacyIO implements IIWLPSLegacyIO {
     slog.info(mmi+"whatTimeIsItNowMinutes="+whatTimeIsItNowMinutes);
 
     // --- Build the YYMMDDhh file prefix name part which is common for all type of
-    //     Spine API legacy ASCII files.
+    //     Spine API legacy ASCII files. NOTE: nowStrISO8601Split[0] -> "YYYY-MM-DDThh"
     String spineAPIInputValuesFName= nowStrISO8601.substring(2,10)
       .replace(ISO8601_YYYYMMDD_SEP_CHAR,"") + nowStrISO8601Split[0].split(ISO8601_DATETIME_SEP_CHAR)[1];
+    
+    //// --- Build the YYMMDDhh file prefix name part which is common for all type of
+    ////     Spine API legacy ASCII files.
+    //String spineAPIInputValuesFName= nowStrISO8601.substring(2,10)
+    //  .replace(ISO8601_YYYYMMDD_SEP_CHAR,"") + nowStrISO8601Split[0].split(ISO8601_DATETIME_SEP_CHAR)[1];
 
     //final Instant mcbLeastRecentInstant= mcbForSpineAPI.get(0).getLeastRecentInstantCopy();
     //slog.info(mmi+"mcbLeastRecentInstant="+mcbLeastRecentInstant.toString());
@@ -119,6 +124,7 @@ public final class IWLPSLegacyIO implements IIWLPSLegacyIO {
     
     // --- Define the daysOffsetInFuture for the Q2, Q3 and Q4 Spine API input files.
     int daysOffsetInFuture= 1;
+    String minutesForSpineApiStr= "00";
 
     // --- 30 days in future case if whatTimeIsItNowMinutes < 15mins
     if (whatTimeIsItNowMinutes < 15) {
@@ -133,28 +139,40 @@ public final class IWLPSLegacyIO implements IIWLPSLegacyIO {
     } else if (whatTimeIsItNowMinutes < 30) {
 
       spineAPIInputValuesFName += "Q2" + QUARTER_FNAME_COMMON_POSTFIX;
+      minutesForSpineApiStr= "15";
 
     } else if (whatTimeIsItNowMinutes < 45) {
 
-      spineAPIInputValuesFName += "Q3" + QUARTER_FNAME_COMMON_POSTFIX; 
+      spineAPIInputValuesFName += "Q3" + QUARTER_FNAME_COMMON_POSTFIX;
+      minutesForSpineApiStr= "30";
 
     } else { // --- 45 <= whatTimeIsItNowMinutes < 59
 	
       spineAPIInputValuesFName += "Q4" + QUARTER_FNAME_COMMON_POSTFIX;
+      minutesForSpineApiStr= "45";
     }
 
     slog.info(mmi+"spineAPIInputValuesFName="+spineAPIInputValuesFName);
+    slog.info(mmi+"minutesForSpineApiStr="+minutesForSpineApiStr);
 
-    // --- Assuming here that all the MeasurementCustomBundle objects of mcbForSpineAPI
-    //     have the same Instant objects values.
-    final Instant mcbLeastRecentInstant= mcbForSpineAPI.get(0).getLeastRecentInstantCopy();
+    // --- Build the ISO8601 date-time string to define the Instant object
+    //     for the Spine API data upload.
+    //     NOTE: nowStrISO8601Split[0] -> "YYYY-MM-DDThh"
+    final String dateTimeForIWLSStr= nowStrISO8601Split[0] +
+      ISO8601_HHMMSS_SEP_CHAR + minutesForSpineApiStr + ISO8601_HHMMSS_SEP_CHAR + "00Z";
 
-    slog.info(mmi+"mcbLeastRecentInstant="+mcbLeastRecentInstant.toString());
+    slog.info(mmi+"dateTimeForIWLSStr="+dateTimeForIWLSStr);
 
-    // --- Get the time intervall in seconds of the adj. FMF WL data.
-    //     (which we assume that it is the same for all the MeasurementCustomBundle objects of mcbForSpineAPI
+    final Instant instantForIWLS= Instant.parse(dateTimeForIWLSStr);
+    
+    //// --- Assuming here that all the MeasurementCustomBundle objects of mcbForSpineAPI
+    ////     have the same Instant objects values.
+    //final Instant mcbLeastRecentInstant= mcbForSpineAPI.get(0).getLeastRecentInstantCopy();
+    //slog.info(mmi+"mcbLeastRecentInstant="+mcbLeastRecentInstant.toString());
+
+    //// --- Get the time intervall in seconds of the adj. FMF WL data.
+    ////     (which we assume that it is the same for all the MeasurementCustomBundle objects of mcbForSpineAPI
     final long mcbTimeIntrvSeconds= mcbForSpineAPI.get(0).getDataTimeIntervallSeconds();
-
     slog.info(mmi+"mcbTimeIntrvSeconds="+mcbTimeIntrvSeconds);
 
     slog.info(mmi+"daysOffsetInFuture="+daysOffsetInFuture);
@@ -162,14 +180,16 @@ public final class IWLPSLegacyIO implements IIWLPSLegacyIO {
     // --- Define the last Instant in the future that is used to define the Instant objects range
     //     for the Spine API depending on the daysOffsetInFuture value.
     final Instant instantsRangeLimitInFuture=
-      mcbLeastRecentInstant.plus(daysOffsetInFuture,ChronoUnit.DAYS);
+      instantForIWLS.plus(daysOffsetInFuture,ChronoUnit.DAYS);
+    //mcbLeastRecentInstant.plus(daysOffsetInFuture,ChronoUnit.DAYS);
 
     slog.info(mmi+"instantsRangeLimitInFuture="+instantsRangeLimitInFuture.toString());
 
-    // --- NOTE: instantsRangeLimitInFuture Instant is not included in the subset of
-    //           Instant objects here.
+    // --- NOTE: New to add plusSeconds(mcbTimeIntrvSeconds) to instantsRangeLimitInFuture
+    //           in order to include it in the wantedInstantsRange
     final SortedSet<Instant> wantedInstantsRange= mcbForSpineAPI.get(0)
-      .getInstantsKeySetCopy().subSet(mcbLeastRecentInstant, instantsRangeLimitInFuture);
+      .getInstantsKeySetCopy().subSet(instantForIWLS, instantsRangeLimitInFuture.plusSeconds(mcbTimeIntrvSeconds));
+    //.getInstantsKeySetCopy().subSet(mcbLeastRecentInstant, instantsRangeLimitInFuture);
 
     // --- Need to add mcbTimeIntrvSeconds to lastInstantWantedInFuture for the SortedSet<Instant>.subSet() method
     //     here in order to include the lastInstantWantedInFuture in this wantedInstantsRange.	
